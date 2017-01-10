0d26853b9e8ebdf32e7e0eede1464b52cb98d0eb
Tjggithub <antonio.gomez@trajimsa.com.mx>
2017-01-10 13:13:10 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 		$uri = 'http://';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
+        header('Location: '.$uri.);	
+/*header('Location: '.$uri.'/dashboard/'); */
 	exit;
 ?>
 Something is wrong with the XAMPP installation :-(
